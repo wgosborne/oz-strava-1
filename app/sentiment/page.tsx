a12a627d72ac32e3115289e9fdc6b0e4de9c6886
Wagner Osborne <wgosborne@crimson.ua.edu>
2025-02-27 23:22:22 -0600

    got sentiment working

--- app/sentiment/page.tsx
+++ app/sentiment/page.tsx
@@ -2,11 +2,33 @@
 
 "use client";
 
+import { WordTokenizer } from "natural";
+import aposToLexForm from "apos-to-lex-form";
+import SpellCorrector from "spelling-corrector";
+import stopword from "stopword";
 import React, { useEffect } from "react";
 import { useStore } from "../store/store";
-
+import Sentiment from "sentiment";
 
 export default function Page() {
+  const getSentiment = (str: string) => {
+    //add spell check and stop word check?
+
+    const sentiment = new Sentiment();
+
+    if (!str.trim()) {
+      return 0;
+    }
+
+    str = str.toLowerCase().replace(/[^a-zA-Z\s]+/g, "");
+
+    let result = sentiment.analyze(str);
+
+    console.log(result);
+  };
+
+  getSentiment("I love you");
+
   return (
     <div>
       <h2>Sentiment Analysis</h2>
